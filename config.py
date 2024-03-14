e1f8b09bb6dfd5f2b5fd7ba00061889678808cd0
stenkjan <m.janstenk@gmail.com>
2024-03-14 14:22:20 +0100

    alpaca_bot

---
+++ config.py
@@ -0,0 +1,7 @@
+# Alpaca API credentials
+PAPER_ALPACA_API_KEY = "${env:alpaca_id}",
+PAPER_ALPACA_SECRET_KEY = "${env:alpaca_sec}",
+
+# Exchange details
+EXCHANGE = "alpaca"  # Or any other supported exchange by CCXT
+SYMBOL = "BTC/USDT"  # Or any other symbol you
